8e1e33a972f8316f88c05f35d9d9157d4144b853
Antonin Bernardin <antonin.bernardin@gmail.com>
2015-10-17 21:19:16 +0200

    Première version séquentielle (CPU) du tatouage d'images avec valeurs aléatoires (problème des valeurs extrêmes d'intensité à résoudre)

--- Tatouage_image_OpenCL/TatouageImage01_seq.cpp
+++ Tatouage_image_OpenCL/TatouageImage01_seq.cpp
@@ -8,43 +8,53 @@ void main() {
 	time_t t = time(NULL);
 	srand((unsigned)&t);
 
-	char imgPath[] = "image/grey.bmp";
+	char imgPath[] = "image/lena.bmp";
 	int width;
 	int height;
 	float* image2D = NULL;
 	image2D = readImage(imgPath, &height, &width);
 	int imgLength = width * height;
 
-	const size_t msgLength = 32;
-	bool* message;
-	message = (bool*)malloc(msgLength);
-	*message = 0b10101010111100110;
+	const size_t msgLength = 7869;
+	bool message[msgLength] = { 1 };
+	bool bit = true;
+	for (int i = 0; i < msgLength; i++) {
+		bit = !bit;
+		message[i] = bit;
+	}
 
-	bool* random;
-	random = (bool*)malloc(imgLength);
-	*random = (2 ^ msgLength) + (rand() * (int)((2 ^ imgLength) - (2 ^ msgLength)) / RAND_MAX);
-	
+	/*unsigned int mask = 0x80000000;
+	unsigned int random = (2 ^ msgLength) + (rand() * (int)((2 ^ imgLength) - (2 ^ msgLength)) / RAND_MAX); // Bad
+
 	int positions[msgLength];
 	int j = 0;
-	for (int i = 0; i < imgLength; i++) {
-		if (random[i] == true) {
+	bool bit;
+	for (int i = 0; i < imgLength; i++) 
+	{
+		mask = mask >> 1;
+		bit = random & mask;
+		if (bit == 1) {
 			positions[j] = i;
 			j++;
 			if (j >= msgLength)
 				break;
 		}
-	}
+	}*/
 	
+	unsigned int maxShift = imgLength / msgLength;
+	unsigned int position = 0;
+	unsigned int shift;
+	float diff = 100.0;
 	for (int i = 0; i < msgLength; i++) {
+		
+		shift = 1 + (rand() * (maxShift - 1) / RAND_MAX);
+		position += shift;
+		bit = message[i];
 
-		int pos = positions[i];
-		bool bitMsg = message[i];
-
-		if (bitMsg == true)
-			image2D[pos] += 1.0;
+		if (bit == true)
+			image2D[position] += diff; //!\ Pb valeurs extrêmes
 		else
-			image2D[pos] -= 1.0;
-
+			image2D[position] -= diff; //!\ Pb valeurs extrêmes
 	}
 
 	storeImage(image2D, "image/output.bmp", height, width, imgPath);
