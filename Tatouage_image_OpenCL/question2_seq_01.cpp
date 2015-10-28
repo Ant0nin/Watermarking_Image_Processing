dfb3176cdcc4a3409ed5d104e4e1a41668b979c5
Antonin Bernardin <antonin.bernardin@gmail.com>
2015-10-28 11:22:39 +0100

    Question 2 version séquentielle 1 clean ;)

--- Tatouage_image_OpenCL/question2_seq_01.cpp
+++ Tatouage_image_OpenCL/question2_seq_01.cpp
@@ -1,74 +1,72 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-
 #include "bmpfuncs.h"
+
+#define ERR_WRONG_INPUT_ARGUMENTS 1
 
 #define MSG_LENGTH 1024
 
-int main() {
+int main(int argc, char *argv[]) {
+
+	const char *imageInputPath;
+	const char *imageOutputPath;
+
+	if (argc == 2 || argc > 3) {
+		printf("Need input image path and output image path as program arguments, or nothing to use default values.\n");
+		system("pause");
+		return ERR_WRONG_INPUT_ARGUMENTS;
+	}
+	else if (argc == 3) {
+		imageInputPath = argv[1];
+		imageOutputPath = argv[2];
+	}
+	else {
+		imageInputPath = "image/lena.bmp";
+		imageOutputPath = "image/output.bmp";
+	}
+
+	int imageWidth, imageHeight;
+	float *image = NULL;
+	image = readImage(imageInputPath, &imageWidth, &imageHeight);
 
 	srand(time(NULL));
 
-	char imgPath[] = "image/lena.bmp";
-	int width;
-	int height;
-	float* image2D = NULL;
-	image2D = readImage(imgPath, &height, &width);
-	int imgLength = width * height;
-
+	// Input
+	int imgLength = imageWidth * imageHeight;
 	const size_t msgLength = MSG_LENGTH;
-	bool message[msgLength]; // 01010101...
+	bool message[msgLength];
 	bool bit = true;
+
+	// Message generation
 	for (int i = 0; i < msgLength; i++) {
+
 		if(i%3==0)
 			bit=1;
 		else
 			bit=0;
-		//bit = !bit;
+
 		message[i] = bit;
-		printf("%d", bit);
 	}
 
-	/*unsigned int mask = 0x80000000;
-	unsigned int random = (2 ^ msgLength) + (rand() * (int)((2 ^ imgLength) - (2 ^ msgLength)) / RAND_MAX); // Bad
-
-	int positions[msgLength];
-	int j = 0;
-	bool bit;
-	for (int i = 0; i < imgLength; i++)
-	{
-	mask = mask >> 1;
-	bit = random & mask;
-	if (bit == 1) {
-	positions[j] = i;
-	j++;
-	if (j >= msgLength)
-	break;
-	}
-	}*/
-	//80 pixeles
-	//10 msg
-	//cada 8
-
-	unsigned int maxShift = imgLength / msgLength; //attention a l'arrondi
+	unsigned int maxShift = imgLength / msgLength;
 	unsigned int position = 0;
 	unsigned int shift;
 	float diff = 1.0;
-	printf("Positions=\n");
+
+	printf("Encoding message...\n");
 	for (int i = 0; i < msgLength; i++) {
 		shift = rand() % maxShift;
 		position = maxShift * i + shift;
 		bit = message[i];
 		if (bit == true)
-			image2D[position] += diff; //!\ Pb valeurs extrêmes
+			image[position] += diff; //!\ Pb valeurs extrêmes
 		else
-			image2D[position] -= diff; //!\ Pb valeurs extrêmes
+			image[position] -= diff; //!\ Pb valeurs extrêmes
 	}
 
-	storeImage(image2D, "image/output.bmp", height, width, imgPath);
+	storeImage(image, imageOutputPath, imageHeight, imageWidth, imageInputPath);
 
 	system("pause");
-	return 0;
+	return EXIT_SUCCESS;
 }
