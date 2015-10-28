5362222ba159aa7aeb2f04d069cf455e29ab28d8
Antonin Bernardin <antonin.bernardin@gmail.com>
2015-10-28 11:12:30 +0100

    Question 1 version séquentielle : Code clean et ajout de la position du pixel.

--- Tatouage_image_OpenCL/question1_seq_01.cpp
+++ Tatouage_image_OpenCL/question1_seq_01.cpp
@@ -1,47 +1,67 @@
-#include <CL/cl.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "bmpfuncs.h"
 
-#define MAX_SOURCE_SIZE (0x100000)
+#define ERR_WRONG_INPUT_ARGUMENTS 1
 
-int main() {
-	
+int main(int argc, char *argv[]) {
 
-	int h,w;
-	float *image_src=NULL;
-	image_src=readImage("image/lena.bmp", &h, &w);
+	const char *imageInputPath;
+	const char *imageOutputPath;
 
-	const int elements = h*w;
+	if (argc == 2 || argc > 3) {
+		printf("Need input image path and output image path as program arguments, or nothing to use default values.\n");
+		system("pause");
+		return ERR_WRONG_INPUT_ARGUMENTS;
+	}
+	else if (argc == 3) {
+		imageInputPath = argv[1];
+		imageOutputPath = argv[2];
+	}
+	else {
+		imageInputPath = "image/lena.bmp";
+		imageOutputPath = "image/output.bmp";
+	}
+
+	int imageWidth, imageHeight;
+	float *image = NULL;
+	image = readImage(imageInputPath, &imageWidth, &imageHeight);
+
+	const int elements = imageHeight*imageWidth;
 	size_t imgsize = sizeof(int)*elements;
 	float *image_dest;
 	image_dest=(float*)malloc(imgsize);
 	
-	printf("Height:%d \n",h );
-	printf("Width:%d \n",w );
 	float max=0;
-	for (int i = 0; i < h; i++)
+	unsigned int pos = 0;
+	for (int i = 0; i < imageHeight; i++)
 	{
-		for (int j = 0; j < w; j++)
+		for (int j = 0; j < imageWidth; j++)
 		{
-			int index = i*w + j;
-			float current_px = image_src[index];
-			//printf("%f \n ", image_src[index]);
-			if(max < current_px)
+			int index = i*imageWidth + j;
+			float current_px = image[index];
+
+			if (max < current_px) {
 				max = current_px;
+				pos = index;
+			}
 
-			/*if (current_px < 255)
-				image_dest[index] = image_src[index] + 1.0f;
-			else*/
-				image_dest[index] = image_src[index]+10;
-
-			//printf("%.2f ,", image_src[i*j+j]);
-		}
-		//printf("\n");
-		
+			image_dest[index] = image[index]+10;
+		}		
 	}
-	printf("Max value:::::::::::::::::::::: %.2f \n",max);
-	storeImage(image_dest, "image/lena_output.bmp", h, w, "image/lena.bmp");
+	unsigned int posX = pos % imageWidth;
+	unsigned int posY = (pos - posX) / imageWidth;
+
+	// Display
+	printf("\n");
+	printf("-------------------------------------------\n");
+	printf("Max intensity pixel: %.1f \n", max);
+	printf("Pixel position: X=%d, Y=%d \n", posX, posY);
+	printf("\n");
+	printf("Quantity of image pixels: %d \n", (int)elements);
+	printf("-------------------------------------------\n");
+	printf("\n");
+
 	system("pause");
+	return EXIT_SUCCESS;
 }
